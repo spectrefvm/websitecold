2604a069cb0c7dc3ef47f64a1c04add037309941
spectrefvm <rtfhffjfhtmimi@googlemail.com>
2025-02-19 06:58:08 +0100

    Create page-2bf1aa9bd3b49696.js

---
+++ _next/static/chunks/app/page-2bf1aa9bd3b49696.js
@@ -0,0 +1,143 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[931], {
+    8201: function(e, t, r) {
+        Promise.resolve().then(r.t.bind(r, 8173, 23)),
+        Promise.resolve().then(r.bind(r, 1477)),
+        Promise.resolve().then(r.bind(r, 2454)),
+        Promise.resolve().then(r.bind(r, 5814)),
+        Promise.resolve().then(r.bind(r, 9323)),
+        Promise.resolve().then(r.bind(r, 9455))
+    },
+    9455: function(e, t, r) {
+        "use strict";
+        r.d(t, {
+            default: function() {
+                return s
+            }
+        });
+        var A = r(7437)
+          , a = r(5272)
+          , i = r(6648)
+          , n = r(2265);
+        function s() {
+            let[e,t] = (0,
+            n.useState)(!1);
+            return (0,
+            A.jsxs)("nav", {
+                className: "flex items-center justify-between bg-[#0c0c0ee5] backdrop-blur-sm shadow-sm h-20 fixed w-full z-10 p-5 xl:px-72",
+                style: {
+                    transform: "none"
+                },
+                children: [(0,
+                A.jsx)("div", {
+                    className: "flex items-center",
+                    children: (0,
+                    A.jsx)("a", {
+                        href: "/",
+                        children: (0,
+                        A.jsx)(i.default, {
+                            src: a.Z,
+                            className: "w-20",
+                            alt: "Logo"
+                        })
+                    })
+                }), (0,
+                A.jsxs)("ul", {
+                    className: "flex items-center space-x-5 md:space-x-8",
+                    children: [(0,
+                    A.jsx)("li", {
+                        className: "hidden md:block",
+                        children: (0,
+                        A.jsx)("a", {
+                            href: "/features",
+                            className: "font-medium relative after:block after:content-[''] after:absolute after:h-[3px] after:bg-base_color after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left py-2 pl-3 pr-4 text-textsecundary rounded hover:bg-gray-400 md:hover:bg-transparent md:hover:text-textprimary md:p-0 md:dark:hover:text-textprimary dark:text-textsecundary transition-all duration-300 ease-in-out",
+                            children: "Features"
+                        })
+                    }), (0,
+                    A.jsx)("li", {
+                        className: "hidden md:block",
+                        children: (0,
+                        A.jsx)("a", {
+                            href: "/#products",
+                            className: "font-medium relative after:block after:content-[''] after:absolute after:h-[3px] after:bg-base_color after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left py-2 pl-3 pr-4 text-textsecundary rounded hover:bg-gray-400 md:hover:bg-transparent md:hover:text-textprimary md:p-0 md:dark:hover:text-textprimary dark:text-textsecundary transition-all duration-300 ease-in-out",
+                            children: "Products"
+                        })
+                    }), (0,
+                    A.jsx)("li", {
+                        className: "text-white",
+                        children: (0,
+                        A.jsx)("a", {
+                            href: "/account/dashboard",
+                            className: "bg-base_color hover:bg-base_hover px-6 py-2 rounded-xl m-1 transition-all duration-300 ease-in-out",
+                            children: "Login"
+                        })
+                    })]
+                })]
+            })
+        }
+    },
+    1477: function(e, t, r) {
+        "use strict";
+        r.r(t),
+        t.default = {
+            src: "/_next/static/media/fivembypass.63df6b85.png",
+            height: 2400,
+            width: 1600,
+            blurDataURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAICAYAAAAx8TU7AAAAs0lEQVR4nAGoAFf/ARMTAADt7QAeMDBSkRERFTPs7OQBAU5OYR3Q0LfGFRVBHCAgLQDw8OkAARYWJqwWFgpT1tbQ+mBglwX6+vUAARISJOsvLhkU6usD/1JScQHi4tUAASgoO+TY2MUbPj5c/1FRYAHHx7cAASkpP9Xq6tYqQ0Nl/D4+SQOvr5cAARwcMuXv7vUaNDU1ACEhKQC8vKUAASMjOiEJCQsu1NS7XAAAADgCAgD85ic+SvKwG74AAAAASUVORK5CYII=",
+            blurWidth: 5,
+            blurHeight: 8
+        }
+    },
+    2454: function(e, t, r) {
+        "use strict";
+        r.r(t),
+        t.default = {
+            src: "/_next/static/media/fivemcheat.4175dc76.png",
+            height: 2400,
+            width: 1600,
+            blurDataURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAICAYAAAAx8TU7AAAAnElEQVR4nA2NsQrCMBiE7xJQbJO0OFmnOrj4Di7i4AvrJgg+QZ+gIIiCgg5NpEoT/+G444PjozH2SWoVB3uo5uaU0hd0bprIHt57FIWD1g7Mc0RjVqjrXWzbo3q9G8+yRHJug0W9xe3eIIRrYJbpZO0S1WyNIY4R/CWIqIzAB13XS0ONxvC01iXZUGoiDEjxB7nnD1INkj2Js3D+AaQwNMSrYnsrAAAAAElFTkSuQmCC",
+            blurWidth: 5,
+            blurHeight: 8
+        }
+    },
+    5814: function(e, t, r) {
+        "use strict";
+        r.r(t),
+        t.default = {
+            src: "/_next/static/media/fivemgame.d6fb2b3d.png",
+            height: 1109,
+            width: 1678,
+            blurDataURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAYAAAB4ka1VAAAAmUlEQVR42mNoqNnPzAAEOdFT5JMi2vVA7KT0wknRCRO6GEAgM6czICtv0uqcpPYzCaEl+9PjDjhFhqV+iozI+xrqVFfLAAIhnl1xztah/33c4/+n+Xe/LMzv/R8fFPs/XiLwLgMMlBZOso0MCbuenzxpUYxr2iZbRbuZOd6zpWHyjCBiTl+N+8ppi/SmR7vpFFi4aZyfd44dABivN8Cb9sqeAAAAAElFTkSuQmCC",
+            blurWidth: 8,
+            blurHeight: 5
+        }
+    },
+    9323: function(e, t, r) {
+        "use strict";
+        r.r(t),
+        t.default = {
+            src: "/_next/static/media/gtavlogo.bf593558.png",
+            height: 1700,
+            width: 2500,
+            blurDataURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAIAAAD38zoCAAAAhUlEQVR42mNQlNbVU7cxUHeyMwnSUjZ3cwjmYJCQkVBh0FQxZWDgyEgv6Ozu7mjvzs7JlxRW4OHlZNBUNvewi+3tmDl/8ex5i2etWLqhJH2CnUkoAxBUl7VXFnampxWFRIZN61uWGddkox/GICIkbW3ioSlvoy5j5uUQk53QnJ/e5GIdDgBU9yU2NuhK8QAAAABJRU5ErkJggg==",
+            blurWidth: 8,
+            blurHeight: 5
+        }
+    },
+    5272: function(e, t) {
+        "use strict";
+        t.Z = {
+            src: "/_next/static/media/logosemfundo.e9d6129a.png",
+            height: 1080,
+            width: 1080,
+            blurDataURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAvElEQVR42mMAgXfv3jP9//+XWYLrGyMDwyfG////MX/48IkJLPnt2zcQAyv4+/cvRO7z5y8R////L4JJ/P//P/7r1285cJW3bj3S/PXry8uZMx7NLiq8O+vPn68vHj167gyWfP78OSuIvnfvdtm5czdenDp1/f6DB3engcTevn3LCjdl6ZL13JcunWu8cuVC++7d+yVBYrt27WIES+7YsR/EQAHHj59BFTt//jzj9+/fGf///8945coVuCQADrJhZBliktIAAAAASUVORK5CYII=",
+            blurWidth: 8,
+            blurHeight: 8
+        }
+    }
+}, function(e) {
+    e.O(0, [648, 971, 23, 744], function() {
+        return e(e.s = 8201)
+    }),
+    _N_E = e.O()
+}
+]);
