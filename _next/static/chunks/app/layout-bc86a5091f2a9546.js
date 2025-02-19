5256ab2374175194cf89507df54203a8d35d1b4c
spectrefvm <rtfhffjfhtmimi@googlemail.com>
2025-02-19 06:57:48 +0100

    Create layout-bc86a5091f2a9546.js

---
+++ _next/static/chunks/app/layout-bc86a5091f2a9546.js
@@ -0,0 +1,40 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[185], {
+    9130: function(n, e, t) {
+        Promise.resolve().then(t.t.bind(t, 1034, 23)),
+        Promise.resolve().then(t.t.bind(t, 3054, 23)),
+        Promise.resolve().then(t.bind(t, 7674))
+    },
+    7674: function(n, e, t) {
+        "use strict";
+        t.d(e, {
+            default: function() {
+                return o
+            }
+        });
+        var r = t(7437)
+          , s = t(998);
+        function o(n) {
+            let {children: e} = n;
+            return (0,
+            r.jsx)(s.SessionProvider, {
+                children: e
+            })
+        }
+    },
+    3054: function() {},
+    1034: function(n) {
+        n.exports = {
+            style: {
+                fontFamily: "'__Inter_36bd41', '__Inter_Fallback_36bd41'",
+                fontStyle: "normal"
+            },
+            className: "__className_36bd41"
+        }
+    }
+}, function(n) {
+    n.O(0, [6, 998, 971, 23, 744], function() {
+        return n(n.s = 9130)
+    }),
+    _N_E = n.O()
+}
+]);
